73135031c1ad34f0d6e18121e73409de98a6a61f
samidahmed2 <samid.ahmed1993@gmail.com>
2026-03-29 23:49:14 +0300

    setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h2 className='text-7xl bg-red-400'>Hello World</h2>
+    </>
+  )
+}
+
+export default App
